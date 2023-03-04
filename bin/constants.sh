94a55e45521bc8ddfac3464aa349d1c8289de755
michaeldelago <michaeldelago98@gmail.com>
2023-03-04 12:56:41 -0500

    split out functions

---
+++ bin/constants.sh
@@ -0,0 +1,8 @@
+# required environment variables
+: "${ASDF_INSTALL_TYPE?}"
+: "${ASDF_INSTALL_VERSION?}"
+: "${ASDF_INSTALL_PATH?}"
+
+toolname="bw"
+old_repository="bitwarden/cli"
+repository="bitwarden/clients"
